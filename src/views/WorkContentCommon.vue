23e6eed1b72bb8e45759207201b53a44af62f418
soojubm <30281891+soojubm@users.noreply.github.com>
2025-01-03 15:56:26 +0900

    pageDetail 정리......

--- src/views/WorkContentCommon.vue
+++ src/views/WorkContentCommon.vue
@@ -7,23 +7,23 @@
     <div class="works-section works-slide-show">
       <div class="works-inner">
         <section class="works-box">
-          <h3 class="project-title" v-html="detail.title"></h3>
-          <p class="project-text" v-html="detail.subtitle"></p>
+          <h3 class="work-title" v-html="detail.title"></h3>
+          <p class="work-subtitle" v-html="detail.subtitle"></p>
           <div class="btn-wrap">
             <!-- <a :href="detail.viewplatform" class="btn-view">View Platform<i class="ico-view"></i></a> -->
           </div>
         </section>
         <section class="works-box">
-          <b class="sub-title">Overview</b>
-          <p class="sub-text" v-html="detail.overview"></p>
+          <b class="work-description-key">Overview</b>
+          <p class="work-description-value" v-html="detail.overview"></p>
           <div class="sub-wrap">
             <div v-for="(value, key) in subDetails" :key="key" class="sub-box">
-              <em class="sub-title">{{ key }}</em>
-              <span class="sub-text" v-html="value"></span>
+              <em class="work-description-key">{{ key }}</em>
+              <span class="work-description-value" v-html="value"></span>
             </div>
-            <div style="margin-top: 30px; width: 100%">
-              <em class="sub-title">UIUX</em>
-              <span class="sub-text" v-html="detail.uiux"></span>
+            <div style="width: 100%">
+              <em class="work-description-key">UIUX</em>
+              <span class="work-description-value" v-html="detail.uiux"></span>
             </div>
           </div>
         </section>
